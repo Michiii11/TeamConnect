309157ab454de4600ee8ddbd0c8e033a6fd1521e
Michiii11 <michael.leisch@gmx.at>
2023-06-05 16:43:35 +0200

    sql file

--- teamconnect.sql
+++ teamconnect.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 02. Jun 2023 um 17:31
--- Server-Version: 10.4.28-MariaDB
--- PHP-Version: 8.2.4
+-- Erstellungszeit: 05. Jun 2023 um 16:43
+-- Server-Version: 10.4.27-MariaDB
+-- PHP-Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,144 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `phpmyadmin`
---
-CREATE DATABASE IF NOT EXISTS `phpmyadmin` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-USE `phpmyadmin`;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `event`
---
-
-CREATE TABLE `event` (
-  `id` int(11) NOT NULL,
-  `teamID` int(11) NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `type` varchar(50) NOT NULL,
-  `duration` int(50) NOT NULL,
-  `result` varchar(100) NOT NULL,
-  `note` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `team`
---
-
-CREATE TABLE `team` (
-  `id` int(11) NOT NULL,
-  `playerID` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `firstname` varchar(50) NOT NULL,
-  `lastname` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `health` varchar(50) NOT NULL,
-  `rule` varchar(50) NOT NULL,
-  `height` int(5) NOT NULL,
-  `weight` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `user_event`
---
-
-CREATE TABLE `user_event` (
-  `eventID` int(11) NOT NULL,
-  `playerID` int(11) NOT NULL,
-  `goal` int(10) NOT NULL,
-  `assist` int(10) NOT NULL,
-  `rating` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `user_team`
---
-
-CREATE TABLE `user_team` (
-  `playerID` int(11) NOT NULL,
-  `teamID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indizes der exportierten Tabellen
---
-
---
--- Indizes für die Tabelle `event`
---
-ALTER TABLE `event`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indizes für die Tabelle `team`
---
-ALTER TABLE `team`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
-
---
--- Indizes für die Tabelle `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indizes für die Tabelle `user_event`
---
-ALTER TABLE `user_event`
-  ADD PRIMARY KEY (`eventID`,`playerID`);
-
---
--- Indizes für die Tabelle `user_team`
---
-ALTER TABLE `user_team`
-  ADD PRIMARY KEY (`playerID`,`teamID`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `event`
---
-ALTER TABLE `event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `team`
---
-ALTER TABLE `team`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
 -- Datenbank: `teamconnect`
 --
-CREATE DATABASE IF NOT EXISTS `teamconnect` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `teamconnect`;
 
 -- --------------------------------------------------------
 
@@ -183,7 +47,11 @@ INSERT INTO `event` (`id`, `teamID`, `date`, `time`, `type`, `description`, `dur
 (17, 1, '2023-06-28', '14:51', 'Fußballtraining', '', 2, NULL, NULL),
 (18, 1, '2023-06-27', '13:12', 'Spiel', '', 2, NULL, NULL),
 (19, 1, '2023-06-23', '14:35', 'Konditionstraining', '', 2, NULL, NULL),
-(23, 1, '2023-06-02', '13:57', 'Fußballtraining', '', 0, NULL, NULL);
+(23, 1, '2023-06-02', '13:57', 'Fußballtraining', '', 0, NULL, NULL),
+(24, 71, '2023-06-02', '21:12', 'Fußballtraining', '', 0, NULL, NULL),
+(25, 71, '2023-06-15', '21:12', 'Fußballtraining', '', 0, NULL, NULL),
+(26, 71, '2023-05-29', '21:12', 'Fußballtraining', '', 0, NULL, NULL),
+(27, 71, '2023-06-14', '02:17', 'Spiel', '', 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -202,18 +70,11 @@ CREATE TABLE `team` (
 --
 
 INSERT INTO `team` (`id`, `playerID`, `name`) VALUES
-(1, 8, 'michis team'),
-(4, 9, 'yaniks team'),
-(18, 8, 'leons team'),
-(19, 8, 'yanik'),
-(20, 8, 'leons'),
-(23, 8, 'test'),
-(25, 9, 'leons teams'),
-(28, 9, 'leons teamer'),
-(29, 9, 'teams'),
-(30, 9, 'taem'),
-(31, 9, 'lköjh'),
-(32, 9, 'stevan vlajic');
+(71, 8, 'michis team'),
+(72, 8, 'lukis team'),
+(86, 9, 'yaniks team'),
+(108, 8, 'nielis team'),
+(119, 10, 'nielis superteam');
 
 -- --------------------------------------------------------
 
@@ -240,7 +101,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `password`, `position`, `health`, `rule`, `height`, `weight`) VALUES
 (8, 'Michael', 'Leisch', 'michael.leisch@gmx.at', '$2y$10$D750CvJDZFfTrwVcfYDF8eO1FCCqb6.YCW8XpgHJHdScWr5Bx6.3a', 'RM', 'Gesund', '', 185, 75),
-(9, 'Yanik', 'Kendler', 'yanik.kendler@gmail.com', '$2y$10$ngrt8eDQB6OT2trgoXE9duRb.VavmNL5DswlY.ooyh4TWfR4Buup.', '', '', '', 0, 0);
+(9, 'Yanik', 'Kendler', 'yanik.kendler@gmail.com', '$2y$10$ngrt8eDQB6OT2trgoXE9duRb.VavmNL5DswlY.ooyh4TWfR4Buup.', '', '', '', 0, 0),
+(10, 'Nieli', 'Nielpferd', 'nieli@nieli.pferd', '$2y$10$5RC83kyWu97K7/yWQnabxOoyS4KDDIeAWhGohDr.1ReGLYm404CXG', 'TW', 'Krank', '', 200, 1000);
 
 -- --------------------------------------------------------
 
@@ -253,17 +115,18 @@ CREATE TABLE `user_event` (
   `playerID` int(11) NOT NULL,
   `goal` int(10) DEFAULT NULL,
   `assist` int(10) DEFAULT NULL,
-  `rating` varchar(50) DEFAULT NULL
+  `yellowCard` int(10) NOT NULL,
+  `redCard` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Daten für Tabelle `user_event`
 --
 
-INSERT INTO `user_event` (`eventID`, `playerID`, `goal`, `assist`, `rating`) VALUES
-(1, 8, NULL, NULL, NULL),
-(2, 8, NULL, NULL, NULL),
-(3, 8, NULL, NULL, NULL);
+INSERT INTO `user_event` (`eventID`, `playerID`, `goal`, `assist`, `yellowCard`, `redCard`) VALUES
+(18, 8, 0, 2, 2, 1),
+(19, 8, 2, 3, 0, 0),
+(24, 8, 3, 2, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -281,28 +144,33 @@ CREATE TABLE `user_team` (
 --
 
 INSERT INTO `user_team` (`playerID`, `teamID`) VALUES
-(8, 0),
-(8, 1),
-(8, 18),
-(8, 19),
-(8, 20),
-(8, 23),
-(8, 29),
-(8, 30),
-(8, 32),
-(8, 33),
-(9, 0),
-(9, 1),
-(9, 18),
-(9, 19),
-(9, 20),
-(9, 23),
-(9, 25),
-(9, 28),
-(9, 29),
-(9, 30),
-(9, 31),
-(9, 32);
+(8, 71),
+(8, 72),
+(8, 108),
+(9, 71),
+(9, 86),
+(10, 119);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user_team_request`
+--
+
+CREATE TABLE `user_team_request` (
+  `id` int(11) NOT NULL,
+  `playerID` int(11) NOT NULL,
+  `teamID` int(11) NOT NULL,
+  `requestTime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `user_team_request`
+--
+
+INSERT INTO `user_team_request` (`id`, `playerID`, `teamID`, `requestTime`) VALUES
+(1, 8, 119, '2023-06-03 18:45:06'),
+(4, 8, 86, '2023-06-03 18:46:54');
 
 --
 -- Indizes der exportierten Tabellen
@@ -340,6 +208,12 @@ ALTER TABLE `user_team`
   ADD PRIMARY KEY (`playerID`,`teamID`);
 
 --
+-- Indizes für die Tabelle `user_team_request`
+--
+ALTER TABLE `user_team_request`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT für exportierte Tabellen
 --
 
@@ -347,24 +221,25 @@ ALTER TABLE `user_team`
 -- AUTO_INCREMENT für Tabelle `event`
 --
 ALTER TABLE `event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT für Tabelle `team`
 --
 ALTER TABLE `team`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
 
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
--- Datenbank: `test`
+-- AUTO_INCREMENT für Tabelle `user_team_request`
 --
-CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `test`;
+ALTER TABLE `user_team_request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
